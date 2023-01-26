cc48c4b09d0762e7c28668c744866cb382320b16
EverBananas <jmd22@student.london.ac.uk>
2023-01-26 21:46:51 +0800

    UI object displaying player lives

---
+++ GroupProject/Assets/scripts/UpdateLives.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class UpdateLives : MonoBehaviour
+{
+    private TextMeshProUGUI livesText;
+
+    // Start is called before the first frame update
+    void Awake() 
+    {
+        livesText = GetComponent<TextMeshProUGUI>();  
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void updateLives()
+    {
+        int numLives = GameManager.instance.playerLives;
+        if(numLives < 0)
+            numLives = 0;
+        livesText.text = "Lives<voffset=0.21em>: <voffset=0em>" + numLives.ToString();
+    }
+}
